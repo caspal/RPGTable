3ca3e421112249996b7e078a256a5f5ffd32b214
Pascal Krause <unknown>
2017-06-15 15:33:40 +0200

    [Initial] Add folder structure and initial project configuration
    Change-Id: I0ce2651e7272760b22afa0b554f37d1932d3168f

---
+++ bin/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Get dir of current script
+bindir="$( cd "$(dirname "$0")" ; pwd -P )"
+projectDir=$(dirname ${bindir})
+
+JAR="${projectDir}/dist/RPGTable.jar"
+CONF="${projectDir}/conf/vertx-conf.json"
+
+java -jar "${JAR}" -conf "${CONF}" "${1}"
