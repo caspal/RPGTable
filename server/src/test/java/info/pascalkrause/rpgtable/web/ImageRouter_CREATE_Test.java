075dc6ff23379f5981e69450cd89475f0e4d9ec6
Pascal <unknown>
2017-06-20 21:49:38 +0200

    Introduce RequestBodyHandler
    Before this change, there was only a handler to reject too large
    requests which was implemented in BodySizeLimitHandler. But now, there
    is also the possibility to check if the request body contains the
    desired MediaType. These two functions are now combined in
    RequestBodyHandler.
    
    To increase the security, this handler does not just parse the
    Content-Type header of the request. The handler really parses the byte
    stream with the help of Tika [1].
    
    [1] https://tika.apache.org/
    
    Change-Id: If968d95242fc7d5feeeee5a776bccc8560a188e2

--- server/src/test/java/info/pascalkrause/rpgtable/web/ImageRouter_CREATE_Test.java
+++ server/src/test/java/info/pascalkrause/rpgtable/web/ImageRouter_CREATE_Test.java
@@ -5,16 +5,20 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 
+import java.io.IOException;
 import java.security.SecureRandom;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.google.common.net.MediaType;
 
 import info.pascalkrause.rpgtable.TestUtils;
+import info.pascalkrause.rpgtable.api.ImageAPI;
 import info.pascalkrause.rpgtable.data.Image;
 import info.pascalkrause.rpgtable.error.BasicError;
 import info.pascalkrause.rpgtable.error.EmptyRequestBodyError;
+import info.pascalkrause.rpgtable.error.InvalidContentTypeError;
 import info.pascalkrause.rpgtable.error.RequestBodyTooLargeError;
 import info.pascalkrause.rpgtable.error.ResourceAlreadyExistError;
 import info.pascalkrause.rpgtable.error.UnexpectedError;
@@ -26,9 +30,15 @@ import io.vertx.ext.unit.TestContext;
 
 public class ImageRouter_CREATE_Test extends ImageRouterTest {
 
+    private static Buffer payload;
+
+    @BeforeClass
+    public static void init() throws IOException {
+        payload = TestUtils.readFile("images/TestImage.gif");
+    }
+
     @Test
     public void testWithName(TestContext context) {
-        Buffer payload = Buffer.buffer("someContent");
         Image expected = new Image("TestName", "somehash", 123, "somePath");
         SimpleAsyncResult<Image> sar = new SimpleAsyncResult<Image>(expected);
         doAnswer(TestUtils.createAsyncResultAnswer(sar)).when(imageApiMock).create(eq(expected.getName()), eq(payload),
@@ -49,7 +59,6 @@ public class ImageRouter_CREATE_Test extends ImageRouterTest {
     @Test
     public void testWithoutName(TestContext context) {
         String key = "ImageRouterCreateTestWithoutName-Image";
-        Buffer payload = Buffer.buffer("someContent");
         doAnswer(TestUtils.createAsyncResultAnswer(i -> {
             Image expexted = new Image(i.getArgument(0).toString(), "somehash", 123, "somePath");
             context.put(key, expexted);
@@ -74,7 +83,6 @@ public class ImageRouter_CREATE_Test extends ImageRouterTest {
         SimpleAsyncResult<Image> sar = new SimpleAsyncResult<Image>(expected);
         doAnswer(TestUtils.createAsyncResultAnswer(sar)).when(imageApiMock).create(any(), any(), any());
 
-        Buffer payload = Buffer.buffer("someContent");
         Async createImage = context.async();
         getClient().post("/v1/images", response -> {
             response.exceptionHandler(context.exceptionHandler());
@@ -106,7 +114,24 @@ public class ImageRouter_CREATE_Test extends ImageRouterTest {
         getClient().post("/v1/images", response -> {
             response.exceptionHandler(context.exceptionHandler());
             response.bodyHandler(body -> {
-                TestUtils.isErrorResponseCorrect(response, body, new RequestBodyTooLargeError(payload.length(), config.BODY_SIZE_LIMIT_BYTES));
+                TestUtils.isErrorResponseCorrect(response, body,
+                        new RequestBodyTooLargeError(payload.length(), config.BODY_SIZE_LIMIT_BYTES));
+                createImage.complete();
+            });
+        }).putHeader(HttpHeaders.CONTENT_TYPE, MediaType.OCTET_STREAM.toString())
+                .putHeader(HttpHeaders.CONTENT_LENGTH, Integer.toString(payload.length())).write(payload).end();
+    }
+
+    @Test
+    public void testThrowInvalidContentTypeError(TestContext context) {
+        Buffer payload = Buffer.buffer("<html>This is not a picture</html>");
+
+        Async createImage = context.async();
+        getClient().post("/v1/images", response -> {
+            response.exceptionHandler(context.exceptionHandler());
+            response.bodyHandler(body -> {
+                TestUtils.isErrorResponseCorrect(response, body, new InvalidContentTypeError(
+                        MediaType.HTML_UTF_8.withoutParameters(), ImageAPI.getSupportedImageTypes()));
                 createImage.complete();
             });
         }).putHeader(HttpHeaders.CONTENT_TYPE, MediaType.OCTET_STREAM.toString())
@@ -118,7 +143,6 @@ public class ImageRouter_CREATE_Test extends ImageRouterTest {
         SimpleAsyncResult<Image> sar = new SimpleAsyncResult<Image>(new Error());
         doAnswer(TestUtils.createAsyncResultAnswer(sar)).when(imageApiMock).create(any(), any(), any());
 
-        Buffer payload = Buffer.buffer("someContent");
         Async createImage = context.async();
         getClient().post("/v1/images", response -> {
             response.exceptionHandler(context.exceptionHandler());
