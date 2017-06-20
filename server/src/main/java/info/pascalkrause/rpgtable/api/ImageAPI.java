1be4ae8e125bacb742e447e43ceeb1551ea9b207
Pascal <unknown>
2017-06-20 21:52:02 +0200

    Introduce end point /v1/images, RPGTableConfig and ImageStore
    This change introduces a new REST end point to list, create, get and
    delete images with the following structure:
    
    GET /v1/images - list all images
    POST /v1/images - create a new image
    GET /v1/images/:nameOrId - get an image
    DELETE /v1/images/:nameOrId - delete an image
    Get /v1/images/:nameOrId/binary - serve the image content
    
    This change also introduces the RPGTableConfig class, which provides
    default configurations. The Vert.x configuration can be used to generate
    the RPGTableConfig.
    
    The only reason for the introduction of the ImageStore, is the
    demonstrability. Otherwise only the tests are working, because they are
    mocking the ImageAPI.
    
    Change-Id: Ife193fe96834f0756b2bfe755dacabe3e9eda74a

---
+++ server/src/main/java/info/pascalkrause/rpgtable/api/ImageAPI.java
@@ -0,0 +1,21 @@
+package info.pascalkrause.rpgtable.api;
+
+import java.util.Collection;
+
+import info.pascalkrause.rpgtable.data.Image;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+
+public interface ImageAPI {
+
+    public void list(Handler<AsyncResult<Collection<Image>>> handler);
+
+    public void create(String name, Buffer buffer, Handler<AsyncResult<Image>> handler);
+
+    public void get(String nameOrId, Handler<AsyncResult<Image>> handler);
+
+    public void delete(String nameOrId, Handler<AsyncResult<Void>> handler);
+    
+    public void getResource(String nameOrId, Handler<AsyncResult<Buffer>> handler);
+}
---
+++ server/src/main/java/info/pascalkrause/rpgtable/utils/RPGTableConfig.java
@@ -0,0 +1,108 @@
+package info.pascalkrause.rpgtable.utils;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableSet;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+public class RPGTableConfig {
+
+    public static class RPGTableConfigOptions {
+        public final static boolean ENV_TEST_DEFAULT = false;;
+        public final static int BODY_SIZE_LIMIT_DEFAULT = 1024 * 1024 * 10; // 10 mebibyte
+        public final static int HTTP_PORT_DEFAULT = 8080;
+
+        private final JsonObject config;
+
+        private RPGTableConfigOptions() {
+            config = new JsonObject();
+            config.put(ENV_TEST_KEY, ENV_TEST_DEFAULT);
+            config.put(BODY_SIZE_LIMIT_BYTES_KEY, BODY_SIZE_LIMIT_DEFAULT);
+            config.put(HTTP_PORT_KEY, HTTP_PORT_DEFAULT);
+            config.put(WORKSPACE_DIR_KEY, System.getProperty(WORKSPACE_DIR_KEY));
+        }
+
+        @VisibleForTesting
+        public static RPGTableConfigOptions defaults() {
+            return new RPGTableConfigOptions();
+        }
+
+        private static RPGTableConfigOptions fromVertx(Vertx vertx) {
+            RPGTableConfigOptions opts = RPGTableConfigOptions.defaults();
+            Map<String, Object> vertxConfMap = vertx.getOrCreateContext().config().getMap();
+            Map<String, Object> optsMap = opts.config.getMap();
+
+            KEYS.parallelStream().filter(key -> vertxConfMap.containsKey(key))
+                    .forEach(key -> optsMap.put(key, vertxConfMap.get(key)));
+            return opts;
+        }
+
+        public RPGTableConfigOptions setEnvTest(boolean envTest) {
+            config.put(ENV_TEST_KEY, envTest);
+            return this;
+        }
+
+        public RPGTableConfigOptions setHttpPort(int httpPort) {
+            config.put(HTTP_PORT_KEY, httpPort);
+            return this;
+        }
+
+        public RPGTableConfigOptions setWorkspaceDir(String workspaceDir) {
+            config.put(WORKSPACE_DIR_KEY, workspaceDir);
+            return this;
+        }
+
+        public RPGTableConfigOptions setBodySizeLimitBytes(int bodySizeLimitBytes) {
+            config.put(BODY_SIZE_LIMIT_BYTES_KEY, bodySizeLimitBytes);
+            return this;
+        }
+    }
+
+    public final static String WORKSPACE_DIR_KEY = "workspace.dir";
+    public final String WORKSPACE_DIR;
+
+    public final static String HTTP_PORT_KEY = "http.port";
+    public final int HTTP_PORT;
+
+    public final static String BODY_SIZE_LIMIT_BYTES_KEY = "http.request.bodySizeLimitBytes";
+    public final int BODY_SIZE_LIMIT_BYTES;
+
+    public final static String ENV_TEST_KEY = "env.test";
+    public final boolean ENV_TEST;
+
+    private final static Set<String> KEYS = ImmutableSet.of(WORKSPACE_DIR_KEY, HTTP_PORT_KEY, BODY_SIZE_LIMIT_BYTES_KEY,
+            ENV_TEST_KEY);
+
+    private final RPGTableConfigOptions options;
+
+    @VisibleForTesting
+    public RPGTableConfig(RPGTableConfigOptions options) {
+        this.options = options;
+        this.ENV_TEST = options.config.getBoolean(ENV_TEST_KEY);
+        this.HTTP_PORT = options.config.getInteger(HTTP_PORT_KEY);
+        this.WORKSPACE_DIR = options.config.getString(WORKSPACE_DIR_KEY);
+        this.BODY_SIZE_LIMIT_BYTES = options.config.getInteger(BODY_SIZE_LIMIT_BYTES_KEY);
+    }
+
+    public static RPGTableConfig getOrCreate(Vertx vertx) {
+        String key = "RPGTableConfigInstance";
+        if (Objects.isNull(vertx.getOrCreateContext().get(key))) {
+            vertx.getOrCreateContext().put(key, new RPGTableConfig(RPGTableConfigOptions.fromVertx(vertx)));
+        }
+        return vertx.getOrCreateContext().get(key);
+    }
+
+    public JsonObject toJsonObject() {
+        return options.config.copy();
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("RPGTable configuration:\n").append(toJsonObject().encodePrettily()).toString();
+    }
+}
