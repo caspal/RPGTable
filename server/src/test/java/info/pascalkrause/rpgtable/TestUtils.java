edea54e3a8ea50c8bbfa2a7eee6bd78bcd713648
Pascal <unknown>
2017-07-16 17:07:05 +0200

    [Image API] Introduce ImageStore
    Introduce ImageStore to create, list, retrieve and remove images.
    
    Change-Id: Ib56bf8c9d756eb14933ab82a90fbf6d00728dc17

---
+++ server/src/test/java/info/pascalkrause/rpgtable/TestUtils.java
@@ -0,0 +1,76 @@
+package info.pascalkrause.rpgtable;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Resources;
+
+import de.flapdoodle.embed.mongo.Command;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.config.io.ProcessOutput;
+import de.flapdoodle.embed.process.runtime.Network;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.TestOptions;
+import io.vertx.ext.unit.report.ReportOptions;
+
+public class TestUtils {
+
+    public static void runTruthTests(TestContext context, Handler<Void> testCode) {
+        try {
+            testCode.handle(null);
+        } catch (Throwable t) {
+            context.fail(t);
+        }
+    }
+    
+    public static Buffer readFile(String ressourcePath) throws IOException {
+        return Buffer.buffer(Resources.asByteSource(Resources.getResource(ressourcePath)).read());
+    }
+
+    public static MongodExecutable prepareMongo(JsonObject mongoConfig) throws UnknownHostException, IOException {
+        Logger logger = LoggerFactory.getLogger(TestUtils.class);
+        String bindIp = mongoConfig.getString("host");
+        int port = mongoConfig.getInteger("port");
+        IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
+                .net(new Net(bindIp, port, false)).build();
+
+        IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder().defaultsWithLogger(Command.MongoD, logger)
+                .processOutput(ProcessOutput.getDefaultInstanceSilent()).build();
+
+        MongodStarter starter = MongodStarter.getInstance(runtimeConfig);
+        return starter.prepare(mongodConfig);
+    }
+
+    public static JsonObject getDefaultMongoConfig() throws IOException {
+        JsonObject mongoConfig = new JsonObject();
+        mongoConfig.put("host", "127.0.0.1");
+        mongoConfig.put("port", Network.getFreeServerPort());
+        mongoConfig.put("db_name", "test");
+        return mongoConfig;
+    }
+
+    public static TestOptions getTestOptions(Vertx vertx) {
+        String reportsPath = "./testreports";
+        if (!vertx.fileSystem().existsBlocking(reportsPath)) {
+            vertx.fileSystem().mkdirBlocking(reportsPath);
+        }
+        TestOptions opts = new TestOptions().addReporter(new ReportOptions().setTo("console"));
+        opts.addReporter(new ReportOptions().setTo("file:./testreports/").setFormat("junit"));
+        opts.addReporter(new ReportOptions().setTo("file:./testreports/").setFormat("simple"));
+        return opts;
+    }
+}
---
+++ server/src/test/java/info/pascalkrause/rpgtable/data/MongoImageStoreCreateTest.java
@@ -0,0 +1,57 @@
+package info.pascalkrause.rpgtable.data;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import info.pascalkrause.rpgtable.TestUtils;
+import info.pascalkrause.rpgtable.error.ResourceAlreadyExistError;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestSuite;
+
+public class MongoImageStoreCreateTest extends AbstractMongoImageStoreTest {
+
+    public MongoImageStoreCreateTest(String suitePrefix, MongoImageStore imageStore, MongoClient mc) {
+        super(suitePrefix + "test_method_create", imageStore, mc);
+    }
+
+    @Override
+    public void addTestCases(TestSuite suite, MongoImageStore testClass, MongoClient mc) {
+        suite.beforeEach(c -> {
+            Async beforeEachComplete = c.async();
+            mc.dropCollection(testClass.getCollectionName(), res -> {
+                testClass.init(res2 -> {
+                    beforeEachComplete.complete();
+                });
+
+            });
+        });
+        suite.test("Create image Test", c -> {
+            Async testComplete = c.async();
+            Image i1 = getTestImage1();
+            testClass.create(i1.name, Buffer.buffer(i1.content), createResult -> {
+                Image created = createResult.result();
+                JsonObject query = new JsonObject();
+                query.put("_id", created.id);
+                mc.find(testClass.getCollectionName(), query, findResult -> {
+                    TestUtils.runTruthTests(c, v -> assertThat(findResult.result().size()).isEqualTo(1));
+                    Image found = MongoImageStore.decode.apply(findResult.result().get(0));
+                    TestUtils.runTruthTests(c, v -> assertThat(found).isEqualTo(created));
+                    testComplete.complete();
+                });
+            });
+        });
+        suite.test("Throw Unique Constraint Exception", c -> {
+            Async testComplete = c.async();
+            Image i1 = getTestImage1();
+            testClass.create(i1.name, Buffer.buffer(i1.content), createResult -> {
+                testClass.create(i1.name, Buffer.buffer(i1.content), createResult2 -> {
+                    TestUtils.runTruthTests(c,
+                            v -> assertThat(createResult2.cause()).isInstanceOf(ResourceAlreadyExistError.class));
+                    testComplete.complete();
+                });
+            });
+        });
+    }
+}
