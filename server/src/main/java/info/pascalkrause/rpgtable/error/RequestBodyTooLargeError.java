6e9559f06756ee9bbf830430e15c223d57da5694
Pascal <unknown>
2017-07-19 22:29:22 +0200

    [Image API] Introduce RequestBodyHandler
    WIP: Tests are missing
    
    RequestBodyHandler is able to reject requests if the body is empty, too
    large or has an unexpected content type.
    
    Change-Id: Id3a581b69a800eb851534008ae9b8ec7fcdb1172

---
+++ server/src/main/java/info/pascalkrause/rpgtable/error/RequestBodyTooLargeError.java
@@ -0,0 +1,10 @@
+package info.pascalkrause.rpgtable.error;
+
+@SuppressWarnings("serial")
+public class RequestBodyTooLargeError extends BasicError {
+
+    public RequestBodyTooLargeError(int actualSize, int maxSize) {
+        super(ErrorType.REQUEST_BODY_TOO_LARGE,
+                "The request body is larger than " + maxSize + " bytes (Limit: " + maxSize + " bytes)");
+    }
+}
