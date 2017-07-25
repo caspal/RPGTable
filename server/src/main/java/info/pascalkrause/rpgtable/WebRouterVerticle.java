b8430ef49aa934fff5387a4e83a2ea81b9b6ad94
Pascal <unknown>
2017-07-25 20:06:46 +0200

    [Image API] Add Image routes to Router
    Change-Id: I17f0615bfe637933f3771f1034a6859fb482ccdc

--- server/src/main/java/info/pascalkrause/rpgtable/WebRouterVerticle.java
+++ server/src/main/java/info/pascalkrause/rpgtable/WebRouterVerticle.java
@@ -2,27 +2,64 @@ package info.pascalkrause.rpgtable;
 
 import java.io.IOException;
 
+import info.pascalkrause.rpgtable.api.ImageAPI;
+import info.pascalkrause.rpgtable.api.ImageApiImpl;
+import info.pascalkrause.rpgtable.data.MongoImageStore;
 import info.pascalkrause.rpgtable.handler.IndexHandler;
+import info.pascalkrause.rpgtable.utils.RPGTableConfig;
+import info.pascalkrause.rpgtable.web.ImageRoutes;
+import info.pascalkrause.vertx.mongodata.datasource.MongoClientDataSource;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Context;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 
 public class WebRouterVerticle extends AbstractVerticle {
+
+    private RPGTableConfig config;
+    private MongoClient mc;
+
+    private Router createRoutes(ImageAPI imageApi) {
+        Router router = Router.router(vertx);
+        router.route("/").handler(new IndexHandler(vertx));
+        router.mountSubRouter("/v1/images", new ImageRoutes(imageApi).createRoutes(vertx));
+        return router;
+    }
+
+    @Override
+    public void init(Vertx vertx, Context context) {
+        super.init(vertx, context);
+        config = RPGTableConfig.getOrCreate(vertx);
+        if (!config.ENV_TEST) {
+            System.out.println(config.toString());
+        }
+        JsonObject mongoConfig = new JsonObject();
+        mongoConfig.put("host", config.MONGO_HOST);
+        mongoConfig.put("port", config.MONGO_PORT);
+        mongoConfig.put("db_name", "RPGTable");
+        mc = MongoClient.createNonShared(vertx, mongoConfig);
+    }
+
     @Override
     public void start(Future<Void> fut) throws IOException {
-        Router router = Router.router(vertx);
-        router.route("/").handler(new IndexHandler(getVertx()));
-
-        // Create the HTTP server and pass the "accept" method to the request handler.
-        vertx.createHttpServer().requestHandler(router::accept).listen(
-                // Retrieve the port from the configuration,
-                // default to 8080.
-                config().getInteger("http.port", 8080), result -> {
-                    if (result.succeeded()) {
-                        fut.complete();
-                    } else {
-                        fut.fail(result.cause());
-                    }
-                });
+        Future<HttpServer> httpServerCompleted = Future.future();
+        Future<MongoImageStore> imageStoreComplete = Future.future();
+        MongoImageStore.createInstance("images", new MongoClientDataSource(mc), imageStoreComplete.completer());
+        imageStoreComplete.compose(mis -> {
+            Router router = createRoutes(new ImageApiImpl(mis));
+            vertx.createHttpServer().requestHandler(router::accept).listen(config.HTTP_PORT,
+                    httpServerCompleted.completer());
+            httpServerCompleted.setHandler(res -> {
+                if (res.succeeded()) {
+                    fut.complete();
+                } else {
+                    fut.fail(res.cause());
+                }
+            });
+        }, httpServerCompleted);
     }
 }
